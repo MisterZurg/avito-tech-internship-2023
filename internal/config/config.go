190905268d7891d0cbe2b991ce4859fc4e26324a
MisterZurg <misterzurg@gmail.com>
2023-08-31 23:52:19 +0300

    Added contracts

--- internal/config/config.go
+++ internal/config/config.go
@@ -6,7 +6,7 @@ type Config struct {
 	DBUser     string `env:"POSTGRES_USER" envDefault:"postgres"`
 	DBPassword string `env:"POSTGRES_PASSWORD" envDefault:"password"`
 	DBName     string `env:"POSTGRES_DB" envDefault:"db"`
-	DBHost     string `env:"POSTGRES_HOST" envDefault:"localhost"` // "192.168.0.108"`
+	DBHost     string `env:"POSTGRES_HOST" envDefault:"localhost"` // "localhost"`
 	DBPort     string `env:"POSTGRES_PORT" envDefault:"5432"`
 }
 
